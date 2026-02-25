5ba5426a002aa566b8f8de73a15fcd6cf219e4f6
Nithin <attilinithin@gmail.com>
2026-02-25 16:11:01 +0530

    made some changes

--- src/main/java/com/example/demo/CSE_controller.java
+++ src/main/java/com/example/demo/CSE_controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CSE_controller {
 	@GetMapping("/home")
 	public String Dis() {
-		return "Welcome to CSE Jenkins";
+		return "Welcome to CSE Jenkins from Aditya University";
 	}
 	@GetMapping("/cse")
 	public String Dis2() {
